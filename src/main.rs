95ce3be2bdbe06d411c68e7ad929b4daeab17fbd
Brian Tenggren <btenggren20@gmail.com>
2019-02-23 19:00:14 -0500

    Use a buffered reader instead of loading the whole file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::BufReader;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    // Show the args given
+    println!("pattern: {}", args.pattern);
+    // Show the path input
+    println!("path: {:?}", args.path);
+    // Get the full path name (returns a result)
+    println!("path (cannonicalized) {:?}", fs::canonicalize(&args.path));
+
+    let f = File::open(args.path).expect("Couldn't open file");
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        let line_contents = line.unwrap();
+        if line_contents.contains(&args.pattern) {
+            println!("{:?}", line_contents);
+        }
+    }
 }
