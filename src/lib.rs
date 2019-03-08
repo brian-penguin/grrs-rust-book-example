ad92979d06536819be8a0fd371fdae716e24ef38
Brian Tenggren <btenggren20@gmail.com>
2019-03-08 16:45:53 -0500

    Write errors to standard error not standard out
    Why:
    * This way errors are surfaced to the terminal not the file
    
    This change is addresses this by:
    * eprintln! macros is super useful

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -15,14 +17,28 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", file_contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &file_contents)
+    } else {
+        search_case_insensitive(&config.query, &file_contents)
+    };
+
+    for line in results {
+        println!("matching line: {}", line);
+    }
     Ok(())
 }
 
@@ -31,6 +47,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     for line in contents.lines() {
         if line.contains(query) {
+            matches.push(line);
+        }
+    }
+
+    matches
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let insensitive_query = query.to_lowercase();
+    let mut matches = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&insensitive_query) {
             matches.push(line);
         }
     }
@@ -52,5 +81,20 @@ Productive for bloots
 bloot's are worth a toot";
 
         assert_eq!(vec!["bloot's are worth a toot"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "blOOt";
+        let contents = "\
+Rust:
+Safe n Fast
+Productive for bloots
+bloot's are worth a toot";
+
+        assert_eq!(
+            vec!["Productive for bloots", "bloot's are worth a toot"],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+use grrs::Config;
 use std::env;
 use std::process;
-use grrs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguements: {}", err);
+        eprintln!("Problem parsing arguements: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = grrs::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
         process::exit(1)
     }
 }
